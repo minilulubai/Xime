6d7eab0e2958e6524b19378a4bb6d5c08d84df1c
kingzcheung <kingzcheung@gmail.com>
2026-06-02 10:27:34 +0800

    bump to v2.3.0-beta3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -308,8 +308,8 @@ android {
         applicationId = "com.kingzcheung.xime"
         minSdk = 28
         targetSdk = 35
-        versionCode = 23
-        versionName = "2.3.0-beta2"
+        versionCode = 24
+        versionName = "2.3.0-beta3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
